685e1d449c9f84fe8cf539a2425c64681367de67
fkornu <franciskornu@gmail.com>
2016-11-18 12:27:34 +0000

    new entry

--- scripts/index.js
+++ scripts/index.js
@@ -50,7 +50,14 @@
 
 	///////////geolocation bit/////////////////
 	var onSuccess = function(position) {
-		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
+		//alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
+		var lat = pos.coords.latitude,
+                long = pos.coords.longitude,
+                coords = lat + ', ' + long;
+
+        document.getElementById('google-map').setAttribute('src', 'http://maps.google.co.uk?q=' + coords + '&z=60&output=embed');
+
+  
 	};
 
 	// onError Callback receives a PositionError object
@@ -62,15 +69,8 @@
 	//watchPosition
 
 	var onWatchSuccess = function(position) {
-		//var element = document.getElementById('divWatchMeMove');
-		var lat = pos.coords.latitude,
-                long = pos.coords.longitude,
-                coords = lat + ', ' + long;
-
-        document.getElementById('google-map').setAttribute('src', 'http://maps.google.co.uk?q=' + coords + '&z=60&output=embed');
-
-  
-		//element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
+		var element = document.getElementById('divWatchMeMove');
+		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
 	};
 
 	function onWatchError(error) {
